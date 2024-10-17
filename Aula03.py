97867a293cad914e3d20766f083a94b8cf90e66a
Lucaasshq <Lucashenriquebarros2016@gmail.com>
2024-10-17 14:35:29 -0300

    initial commit

---
+++ Aula03.py
@@ -0,0 +1,8 @@
+nota_txt = input()
+nota1 = float(nota_txt)
+
+nota_txt2 = input()
+nota2 = float(nota_txt2)
+
+media = (nota1*2+nota2*3) / 5
+print(media)
---
+++ Aula05.py
@@ -0,0 +1,5 @@
+nota1, nota2 = map(int, input().split());
+media = nota1*2+nota2*3;
+print(media);
+
+print(numero)
