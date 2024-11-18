f3541a9b230b0ef243a5f4a8a91d539d9ffab180
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-11-18 20:53:32 -0300

    Resolvendo mais exercicios

--- 5-lista-codeforces/G-andando-no-tempo.py
+++ 5-lista-codeforces/G-andando-no-tempo.py
@@ -1,3 +1,10 @@
-A,B,C = map(int, input().split(" "))
+
+A, B, C = map(int, input().split())
 
 
+if (A - C == 0) or (A - B == 0) or (B - C == 0) or (B - A == 0) or (C - A == 0) or (C - B == 0): 
+    print("S")
+elif(A == (B + C)) or (B == (A + C) or (C == (B + A))):
+    print("S")
+else:
+    print("N")
--- 5-lista-codeforces/I-maquina-de-cafe.py
+++ 5-lista-codeforces/I-maquina-de-cafe.py
@@ -2,8 +2,10 @@ a1 = int(input())
 a2 = int(input())
 a3 = int(input())
 
-r1 = a1 * 2
-r3 = a3 * 2
 
-soma=r1+r3
-print(soma)
+tempo1 = a2 * 2 + a3 * 4
+tempo2 = a1 * 2 + a3 * 2
+tempo3 = a1 * 4 + a2 * 2
+
+
+print(min(tempo1, tempo2, tempo3))  
