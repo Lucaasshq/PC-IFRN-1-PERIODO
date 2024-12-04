9c8cddcf206e15e35f78a103d79c537c3c296973
LUCAS HENRIQUE BARROS MEDEIROS <20242014040042@ifrn.local>
2024-12-04 15:46:13 -0300

    ex

---
+++ fdfdf.py
@@ -0,0 +1,4 @@
+a, b, c = map(int, input().split())
+
+media = ((a * 2) + (b * 3) + (c * 5)) / (2+3+5)
+print(media)
--- testes.py
+++ testes.py
@@ -1,15 +1,22 @@
-a = int(input())
-b = int(input())
-c = int(input())
 
-maior = 0
-
-if(a > b and a > c):
-  maior = a
-elif(b > a and b > c):
-  maior = b
-elif(c > a and c > b):
-  maior = c
+a, b, c = map(int, input().split())
 
 
-print(maior)
+if a > b and a > c:
+    maior = a
+    lado1 = b
+    lado2 = c
+elif b > a and b > c:
+    maior = b
+    lado1 = a
+    lado2 = c
+else:
+    maior = c
+    lado1 = a
+    lado2 = b
+
+
+if maior**2 == lado1**2 + lado2**2:
+    print("Os valores formam um triângulo retângulo.")
+else:
+    print("Os valores não formam um triângulo retângulo.")
