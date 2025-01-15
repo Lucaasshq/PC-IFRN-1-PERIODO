77fb28b6ea7fcf983ce7f0124f03460df9da9842
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2025-01-15 20:35:50 -0300

    Update B-meia-vida.py

--- 8-lista-codeforces/B-meia-vida.py
+++ 8-lista-codeforces/B-meia-vida.py
@@ -1,3 +1,22 @@
-tempo,massa = map(int, input().split())
+def tempo_para_massa_menor_05(s, mI):
+    tempo_total = 0
+    massa = mI
 
-print("Teste")
+    while massa >= 0.5:
+        massa = massa / 2
+        tempo_total = tempo_total + s
+
+    dias = tempo_total // 86400
+    tempo_total = tempo_total % 86400
+
+    horas = tempo_total // 3600
+    tempo_total = tempo_total % 3600
+
+    minutos = tempo_total // 60
+    segundos = tempo_total % 60
+
+    print(f"{dias} dias {horas:2}:{minutos:2}:{segundos:2}")
+
+    s, mI = map(int, input().split(" "))
+
+    tempo_para_massa_menor_05(s, mI)
