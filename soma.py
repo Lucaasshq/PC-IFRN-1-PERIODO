5c5660d8d0fc42e5946b2364a76924bbf71f540e
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2025-02-16 20:28:52 -0300

    hghf

--- soma.py
+++ soma.py
@@ -1,4 +1,11 @@
-def dobro(num):
-    return num * 2
-  
-print(dobro(5)) # 10
+def numero_rec(lista, i=0):
+    if i == len(lista):
+        return 0  # Retorna 0 se a lista estiver vazia
+
+    if lista[i] % 2 == 0:
+        return 1 + numero_rec(lista, i+1)  # Conta o número par
+    else:
+        return numero_rec(lista, i+1)  # Apenas chama recursivamente sem contar
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(numero_rec(numeros, 0))
